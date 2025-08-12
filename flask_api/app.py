72b27cc68a2c341d905f482586e97714902d8186
rndmtrsh <t.maulanaakmal@gmail.com>
2025-08-12 13:43:59 +0700

    Online Deploy

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from routes import bp  
+from routes import bp
 
 def create_app():
     app = Flask(__name__)
@@ -8,6 +8,7 @@ def create_app():
     app.register_blueprint(bp)
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=False)
